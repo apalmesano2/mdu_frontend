5ee154c807e43d16dce11b739d030745ced23f5c
apalmesano2 <andrewpalm22@gmail.com>
2019-08-04 21:47:52 -0500

    added weather forecasts

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -7,7 +7,7 @@
           Welcome {{validUserName}}!
           <footer>
             <small>
-              <em>&mdash;Check out how the stock market is doing!</em>
+              <em>&mdash; Enter a zip code to see weather data for that area</em>
             </small>
           </footer>
         </blockquote>
@@ -16,41 +16,90 @@
       <v-container grid-list-md fill-height>
         <v-layout wrap>
           <v-flex md12>
-            <v-card v-if="authenticated" class="mb-4 mx-auto">
-              <p class="headline mx-4 pt-3 mb-0">Your Market Tracker's Previous 6 Days:</p>
+            <v-card class="mb-2 mx-auto">
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <v-layout row justify-center>
+                  <v-flex md11>
+                    <v-text-field
+                      class="ml-5"
+                      label="Zip Code"
+                      v-model="zipCode"
+                      :rules="rules.zipCode"
+                      maxlength="5"
+                    />
+                  </v-flex>
+                  <v-flex md1>
+                    <v-btn
+                      class="mt-4 ml-5 blue darken-4"
+                      :disabled="!valid"
+                      @click="getWeather(zipCode)"
+                    >Submit</v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-form>
+            </v-card>
+          </v-flex>
+          <v-flex md12>
+            <v-card v-if="zipEntered" class="mb-1">
+              <div class="headline ml-4 pt-2">Current Weather for {{ zipCode }}</div>
+              <hr class="mb-2" />
+              <v-layout row>
+                <v-layout column class="ml-5 mb-2">
+                  <div>{{ currentWeather.weather.description }}</div>
+                  <div>Temperature: {{ currentWeather.temp }} &deg;C</div>
+                  <div>Feels Like: {{ currentWeather.app_temp }} &deg;C</div>
+                  <div>Wind: {{ currentWeather.wind_spd.toFixed(1) }} m/s {{ currentWeather.wind_cdir_full }}</div>
+                  <div>Humidity: {{ currentWeather.rh }}%</div>
+                </v-layout>
+                <v-layout column>
+                  <v-img
+                    :src="require(`@/assets/images/weather_icons/${currentWeather.weather.icon}.png`)"
+                    height="100px"
+                    contain
+                  />
+                </v-layout>
+                <v-layout column />
+                <v-layout column />
+                <v-layout column />
+                <v-layout column />
+                <v-layout column />
+                <v-layout column />
+              </v-layout>
+            </v-card>
+          </v-flex>
+          <v-flex md-12>
+            <v-card v-if="zipEntered">
+              <div class="headline ml-4 pt-2">Forecast for {{ zipCode }}</div>
               <hr class="mb-2" />
               <v-layout wrap>
-                <template v-for="day in tracker">
-                  <v-flex :key="day" md2>
-                    <v-card :key="day" class="mx-3 mb-2 pa-2">
-                      <p class="mb-0 title">{{ day[0] }}</p>
-                      <hr class="mb-1" />
-                      <p class="mb-0">Open: ${{ day[1]['1. open'] }}</p>
-                      <p class="mb-0">High: ${{ day[1]['2. high'] }}</p>
-                      <p class="mb-0">Low: ${{ day[1]['3. low'] }}</p>
-                      <p>Close: ${{ day[1]['4. close'] }}</p>
+                <template v-for="day in forecast">
+                  <v-flex :key="day" md3>
+                    <v-card class="mx-auto" :key="day">
+                      <div class="title ml-4">{{ day.valid_date }}</div>
+                      <hr class="mb-2" />
+                      <v-layout wrap>
+                        <v-layout column class="ml-5 mb-2">
+                          <div>{{ day.weather.description }}</div>
+                          <div>Low: {{ day.min_temp }} &deg;C</div>
+                          <div>High: {{ day.max_temp }} &deg;C</div>
+                          <div>Wind: {{ day.wind_spd.toFixed(1) }} m/s {{ day.wind_cdir_full }}</div>
+                          <div>Humidity: {{ day.rh }}%</div>
+                        </v-layout>
+                        <v-layout column>
+                          <v-img
+                            :src="require(`@/assets/images/weather_icons/${day.weather.icon}.png`)"
+                            height="75px"
+                            contain
+                            class="mb-5"
+                          />
+                        </v-layout>
+                      </v-layout>
                     </v-card>
                   </v-flex>
                 </template>
               </v-layout>
             </v-card>
-            <v-card class="mx-auto">
-              <p class="headline mx-4 pt-3 mb-0">{{ sectors['Meta Data']['Information'] }}</p>
-              <p class="mb-1 mx-4 pb-2">Last Refreshed: {{ this.sectors['Meta Data']['Last Refreshed'] }}</p>
-            </v-card>
           </v-flex>
-          <template v-for="group in groups">
-            <v-flex :key="group" md4>
-              <v-card :key="group">
-                <p class="headline mx-4 pt-3 mb-0">{{ group[0].substring(8) }}</p>
-                <hr class="mb-2" />
-                <template v-for="sector in group[1]">
-                  <div :key="sector" class="mx-4">{{ sector[0] }}: {{ sector[1] }}</div>
-                </template>
-                <br />
-              </v-card>
-            </v-flex>
-          </template>
         </v-layout>
       </v-container>
     </v-container>
@@ -64,23 +113,29 @@ import { APIService } from "../http/APIService";
 const apiService = new APIService();
 
 export default {
-  name: "Stocks",
+  name: "Weather",
   data: () => ({
-    sectors: [],
-    groups: [],
-    tracker: [],
+    valid: true,
     authenticated: false,
+    zipCode: "",
+    zipEntered: false,
+    currentWeather: [],
+    forecast: [],
     validUserName: "Guest",
     showMsg: "",
-    headers: []
+    headers: [],
+    rules: {
+      zipCode: [
+        v => (v && v.length === 5) || "Zip code must be 5 digits",
+        v => /^[0-9]+$/.test(v) || "A username can only contain numbers"
+      ]
+    }
   }),
   mounted() {
-    this.getSectors();
     this.showMessages();
     if (localStorage.getItem("isAuthenticates")) {
       this.authenticated = true;
       this.validUserName = JSON.parse(localStorage.getItem("log_user"));
-      this.getMarketTracker();
     }
   },
   methods: {
@@ -90,79 +145,27 @@ export default {
         this.showMsg = this.$route.params.msg;
       }
     },
-    getSectors() {
-      apiService
-        .getSectorData()
-        .then(response => {
-          this.sectors = response.data;
-          const res = Object.entries(response.data);
-          res.shift();
-          res.forEach(element => {
-            element[1] = Object.entries(element[1]);
-          });
-          this.groups = res;
-        })
-        .catch(error => {
-          if (error.response.status === 401) {
-            localStorage.removeItem("isAuthenticates");
-            localStorage.removeItem("log_user");
-            localStorage.removeItem("token");
-            router.push("/auth");
-          }
+    getWeather(zip) {
+      this.zipEntered = true;
+      apiService.getCurrentWeather(zip).then(response => {
+        this.currentWeather = response.data.data[0];
+      });
+
+      apiService.getForecast(zip).then(response => {
+        let temp = response.data.data;
+        temp.forEach(item => {
+          item.valid_date = new Date(item.valid_date);
+          item.valid_date = item.valid_date.toDateString();
         });
-    },
-    getMarketTracker() {
-      apiService
-        .getMarketTracker()
-        .then(response => {
-          const respData = Object.entries(response.data["Time Series (Daily)"]);
-          for (let i = 100; i > 6; i--) {
-            respData.pop();
-          }
-          respData.forEach(item => {
-            item[0] = new Date(item[0]).toDateString();
-
-            //debugger;
-            item[1]["1. open"] = parseFloat(item[1]["1. open"]);
-            item[1]["1. open"] = item[1]["1. open"].toFixed(2);
-            item[1]["1. open"] = item[1]["1. open"].replace(
-              /\B(?=(\d{3})+(?!\d))/g,
-              ","
-            );
-
-            item[1]["2. high"] = parseFloat(item[1]["2. high"]);
-            item[1]["2. high"] = item[1]["2. high"].toFixed(2);
-            item[1]["2. high"] = item[1]["2. high"].replace(
-              /\B(?=(\d{3})+(?!\d))/g,
-              ","
-            );
-
-            item[1]["3. low"] = parseFloat(item[1]["3. low"]);
-            item[1]["3. low"] = item[1]["3. low"].toFixed(2);
-            item[1]["3. low"] = item[1]["3. low"].replace(
-              /\B(?=(\d{3})+(?!\d))/g,
-              ","
-            );
-
-            item[1]["4. close"] = parseFloat(item[1]["4. close"]);
-            item[1]["4. close"] = item[1]["4. close"].toFixed(2);
-            item[1]["4. close"] = item[1]["4. close"].replace(
-              /\B(?=(\d{3})+(?!\d))/g,
-              ","
-            );
-          });
-          this.tracker = respData;
-        })
-        .catch(error => {
-          if (error.response.status === 401) {
-            this.authenticated = false;
-            localStorage.removeItem("isAuthenticates");
-            localStorage.removeItem("log_user");
-            localStorage.removeItem("token");
-            router.push("/auth");
-          }
-        });
+        this.forecast = temp;
+      });
     }
   }
 };
 </script>
+<style>
+.v-content {
+  height: 100%;
+  background-color: #303030;
+}
+</style>
--- src/http/APIService.js
+++ src/http/APIService.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 const API_URL = 'http://mydailyupdate.herokuapp.com'; /* http://groyce.pythonanywhere.com/ or http://localhost:8000 */
 const NEWS_API = '5e1e0ae2beea466f8673c0ab1ae181f8';
-const STOCK_API = '5MHUFOYQCBAHIL3Z'
+const STOCK_API = '5MHUFOYQCBAHIL3Z';
+const WEATHER_API = '32bad162ffbf4942ad533f40ca430f9f';
 
 export class APIService {
   constructor() {
@@ -58,6 +59,18 @@ export class APIService {
 
   getMarketTracker() {
     return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=^DJI&outputsize=compact&apikey=${STOCK_API}`);
+  }
+
+  getCurrentWeather(zipcode) {
+    const url = `http://api.weatherbit.io/v2.0/current?key=${WEATHER_API}&postal_code={ZIP_CODE}&country=US`;
+    const newURL = url.replace('{ZIP_CODE}', zipcode);
+    return axios.get(newURL);
+  }
+
+  getForecast(zipcode) {
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API}&postal_code={ZIP_CODE}&country=US`;
+    const newURL = url.replace('{ZIP_CODE}', zipcode);
+    return axios.get(newURL);
   }
 
   authenticateLogin(credentials) {
